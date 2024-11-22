5164c11dedd10093367ac039cf6613f53d8c6639
hiddevb <hiddevanbavel@hotmail.com>
2024-11-22 10:36:17 +0100

    fix ph_waarde: add solutions

--- exercises/modules-math-ph/solution/solution.py
+++ exercises/modules-math-ph/solution/solution.py
@@ -1,12 +1,4 @@
-def nog_te_zetten(stappen):
-    nog_te_zetten = 10000 - stappen
-    return nog_te_zetten
+import math
 
-def stappenteller(stappen):
-    steps_to_make = nog_te_zetten(stappen)
-    if steps_to_make == 1:
-        print('Je moet nog maar één stap zetten!')
-    elif steps_to_make <= 0:
-        print('Je hebt je stappendoel bereikt!')
-    else:
-        print('Je dient nog', steps_to_make, 'stappen te zetten.')
+def ph_waarde(concentratie):
+  return math.floor(- math.log(concentratie, 10))
